1a5a3628360bf39dd234925bdb4d3ff4cd09f4c3
India <india.kaurbrown@zoopla.co.uk>
2020-08-26 15:59:51 +0100

    Final commit

--- src/shop.ts
+++ src/shop.ts
@@ -27,24 +27,9 @@ const isConjuredItem = (item: Item): boolean => {
     return item.name === 'Conjured Items';
 }
 
-// const isRegularItem = (item: Item): boolean => {
-//     return !isLegendary(item) && !isAgedBrie(item) && !isBackstagePass(item)
-// }
-
-// const isNotAgedBrieOrBackstagePass = (item: Item): boolean => {
-//     return !isAgedBrie(item) && !isBackstagePass(item)
-// }
-
-// const isNotLegendaryItem = (item: Item): boolean => {
-//     return !isAgedBrie(item) && !isBackstagePass(item)
-// }
-
-
 
 const getUpdatedRegularItemQuality = (item: Item): number => {
     let newQuality = item.quality - 1;
-
-
     if (isExpired(item)) {
         newQuality--;
     }
@@ -60,19 +45,15 @@ const getUpdatedBackstagePassQuality = (item: Item): number => {
     if (item.sellIn < 11) {
         newQuality++;
     }
-
     if (item.sellIn < 6) {
         newQuality++;
     }
     if (newQuality >= 50) {
         newQuality = 50;
     }
-
     if (item.sellIn <= 0) {
         newQuality = 0;
     }
-
-    
     if (isExpired(item)) {
         newQuality = 0
     }
@@ -97,9 +78,7 @@ const getUpdatedAgedBrieQuality = (item: Item): number => {
     return newQuality;
 }
 
-
 const getUpdatedQuality = (item: Item): number => {
-
 
     if (isLegendary(item)) {
         return item.quality;
@@ -107,11 +86,9 @@ const getUpdatedQuality = (item: Item): number => {
     if (isAgedBrie(item)) {
         return getUpdatedAgedBrieQuality(item);
     }
-
     if (isBackstagePass(item)) {
         return getUpdatedBackstagePassQuality(item);
     }
-
     if (isConjuredItem(item)) {
         return getUpdatedConjuredItemQuality(item);
     }
@@ -121,7 +98,6 @@ const getUpdatedQuality = (item: Item): number => {
 
 const getUpdatedConjuredItemQuality = (item: Item): number => {
     let newQuality = item.quality - 2;
-
     if (isExpired(item)) {
         newQuality = newQuality - 2
     }
@@ -135,8 +111,6 @@ export const updateQuality = (items: Item[]): Item[] => {
     items.forEach((item: Item) => {
         item.quality = getUpdatedQuality(item);
         item.sellIn = getUpdatedSellIn(item);
-
     });
-
     return items;
 }
